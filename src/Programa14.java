0313e24b7f69c7f0bd45f98a599b3484aa06af55
cristianoalice <cristiano.francoalice@gmail.com>
2019-10-24 18:07:04 -0300

    curso_java_24.10

---
+++ src/Programa14.java
@@ -0,0 +1,35 @@
+// Tipos booleanos
+
+public class Programa14 {
+	
+	public static void main(String[] args) {
+		//Tipos primitivos
+		boolean verdadeiro = true;
+		boolean falso = false;
+		
+				
+		//Tipos não primitivos
+		Boolean v = true;
+		Boolean f = false;
+		
+		System.out.println("Verdadeiro " + verdadeiro);
+		System.out.println("Falso " + falso);
+		System.out.println("Verdadeiro " + v);
+		System.out.println("Falso " + f);
+		
+		if (verdadeiro) {
+			System.out.println("É verdadeiro");
+		}else{
+			System.out.println("É falso");
+		}
+		
+		if (1 == 1) {
+			System.out.println("É verdadeiro");
+		}else{
+			System.out.println("É falso");
+		}
+		
+		
+	}
+
+}
