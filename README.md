e306e2868a407fabf13159da074ae698f15a0358
cristianoalice <cristiano.francoalice@gmail.com>
2019-10-27 23:07:13 -0300

    ultimate.27.10

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
-
+﻿
 # curso-Java
 
+Em Casa: git clone https://github.com/cristianoalice/curso-Java.git
 
-Em Casa: git clone git@github.com:cristianoalice/curso-Java.git
 
-Obs.: tive que usar o https://github.com/cristianoalice/curso-Java.git
+A) Abra o Git Bash e vá até a pasta onde está o seu projeto.
+D:\curso_Java_Ultimate
+
+B) Agora, transformar essa minha pasta em um repositório Git.
+Pra isso, basta digitar git init e dar enter.
+
+C) Para adicionar uma origin, executar o comando:
+git remote add origin https://github.com/cristianoalice/curso_Java_Ultimate.git
+
+D) git status 
+Quando houver um arquivo novo, ele mostrará que o arquivo não está monitorado pelo Git.
+
+E) git add * ou git add "nome do arquivo + extensão"
 
 
 
---
+++ src/Programa04.java
@@ -0,0 +1,35 @@
+﻿import java.util.Scanner;
+
+// exercício 4
+
+public class Programa04 {
+	public static void main(String[] args) {
+		int num1, num2, num3, num4;
+		Scanner teclado = new Scanner (System.in);
+		
+		
+		System.out.println("Quantas vezes vocês surfou na 1ª semana do mês?");
+		num1 = Integer.parseInt(teclado.nextLine());
+		
+		System.out.println("Quantas vezes vocês surfou na 2ª semana do mês?");
+		num2 = Integer.parseInt(teclado.nextLine());
+		
+		System.out.println("Quantas vezes vocês surfou na 3ª semana do mês?");
+		num3 = Integer.parseInt(teclado.nextLine());
+		
+		System.out.println("Quantas vezes vocês surfou na última semana do mês?");
+		num4 = Integer.parseInt(teclado.nextLine());
+		
+		System.out.println("Você surfou " + (num1 + num2 + num3 + num4) + " vezes no mês passado.");
+		
+		if (num1 + num2 + num3 + num4 > 15) {
+			System.out.println("Parabéns!! Você surfou bastante!! Continue assim...");
+		}else {
+			System.out.println("Legal!! Mas você pode mais!! Vá remar...");
+		}
+				
+			
+			
+	}
+
+}
